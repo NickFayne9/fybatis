bda9e7fde42206362bdf30a4a7d1413951ecc7c5
xiadonghui <xiadonghui@flksec.com>
2018-05-04 09:19:27 +0800

    '提交v1.0.0代码'

--- src/main/java/com/faynely/fybatis/IStudentMapper.java
+++ src/main/java/com/faynely/fybatis/IStudentMapper.java
@@ -3,6 +3,6 @@ package com.faynely.fybatis;
 /**
  * @author Student 表操作类型接口 2018-05-03 23:00
  */
-public interface StudentMapper {
+public interface IStudentMapper {
     Student selectStuById(Integer id);
 }
--- src/main/java/com/faynely/fybatis/SqlSession.java
+++ src/main/java/com/faynely/fybatis/SqlSession.java
@@ -1,7 +1,7 @@
 package com.faynely.fybatis;
 
 /**
- * Sql 会话类
+ * Sql 会话类（提供给用户使用）
  * @author Faynely 2018-05-03 22:47
  */
 public class SqlSession {
@@ -10,7 +10,7 @@ public class SqlSession {
      * 获取操作 Student 表的 Mapper
      * @return
      */
-    public static StudentMapper getStudentMapper(){
+    public static IStudentMapper getStudentMapper(){
         return new StudentMapperImpl();
     }
 }
